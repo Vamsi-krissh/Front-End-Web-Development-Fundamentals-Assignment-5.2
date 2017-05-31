054309d3de8a2f87c012af3e4a92166eaaa5d975
Vamsi-krissh <vamsi1393@gmail.com>
2017-05-31 16:42:40 +0530

    Assignment 5.2

---
+++ app1.js
@@ -0,0 +1,8 @@
+
+function myfunction(){
+    
+    //Object for storing personal Details
+var persondetails = {name:'Vamsi',age:25,placeofbirth:"Hyderabad",dob:"13/07/1993"}
+
+console.log("Name:"+ persondetails.name+"\n"+"Age:"+persondetails.age+"\n"+"Dob:"+persondetails.dob+"\n"+"Place of birth:"+persondetails.placeofbirth)
+};
